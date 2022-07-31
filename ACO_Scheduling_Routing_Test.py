d2ef2f1a43867870aaa4c367814b98eed864c309
Rebecca-lee345 <63398186+Rebecca-lee345@users.noreply.github.com>
2022-07-31 22:11:40 +0200

    updated route planning

--- ACO_Scheduling_Routing_Test.py
+++ ACO_Scheduling_Routing_Test.py
@@ -23,7 +23,9 @@ BETA:Beta值越大，蚁群越就容易选择局部较短路径，这时算法�
 Ant_num = 6
 
 # Input task list
-Task_list = pd.read_excel('Task_list.xlsx', sheet_name='Tasklist', usecols="A:G", skiprows=0, nrows=141, dtype=object)
+#Task_list = pd.read_excel('Task_list.xlsx', sheet_name='Tasklist', usecols="A:G", skiprows=0, nrows=141, dtype=object)
+Task_list = pd.read_excel('Task_list_Test.xlsx', sheet_name='Tasklist', usecols=[0, 3, 6, 7, 8], skiprows=0, nrows=6,
+                          dtype=object)
 Task_list_Forklift = Task_list.loc[Task_list['Task type'] == 'C04_CMD']
 Task_list_AGV = Task_list.loc[Task_list['Task type'] != 'C04_CMD']
 
@@ -39,7 +41,7 @@ H = list(range(0, 600))  # the avaulable time (s), for now 1 hour
 # pheromone matrix & Ant visibility matrix
 pheromone_graph = [[1.0 for col in Tasks] for raw in Tasks]
 visibility_graph = [[0.0 for col in Tasks] for raw in Tasks]
-
+'''
 # 创建一个txt文件，文件名为mytxtfile
 def text_create(name):
     desktop_path = "C:\\Users\\Administrator\\PycharmProjects\\EmotionRecog\\venv\\Scripts\\src\\mylog\\"
@@ -52,7 +54,7 @@ text_create(filename)
 output = sys.stdout
 outputfile = open("C:\\Users\\Administrator\\PycharmProjects\\EmotionRecog\\venv\\Scripts\\src\\mylog\\" + filename + '.txt', 'w')
 sys.stdout = outputfile
-
+'''
 # -----Ants-----
 class Ant(object):
     # Initial
@@ -291,7 +293,7 @@ class SCHEDULING(object):
                                         temp_prob -= select_tasks_prob[i]
                                         if temp_prob < 0.0:
                                             next_task = i
-                                            print(next_task)
+                                            #print(next_task)
                                             break
                                     else:
                                         continue
@@ -312,12 +314,23 @@ class SCHEDULING(object):
                         # if ant.total_distance < self.best_ant.total_distance:
                         # 更新最优解
                         # self.best_ant = copy.deepcopy(ant)
-                        print(u"the index of iteration：", self.iter, u"Vehicle task list：", ant.path, ant.current_task,
-                              u"Total completion time：", self.total_distance)
+                        #print(u"the index of iteration：", self.iter, u"Vehicle task list：", ant.path, ant.current_task,
+                              #u"Total completion time：", self.total_distance)
                     else:
                         ant.path.append(next_task)
-                        print(u"the index of iteration：", self.iter, u"Vehicle task list：", ant.path, ant.current_task,
-                              u"Total completion time：", self.total_distance)
+                        #print(u"the index of iteration：", self.iter, u"Vehicle task list：", ant.path, ant.current_task,
+                              #u"Total completion time：", self.total_distance)
+            #vehicle_tasklist = {}
+            vehicle_tasklist = {1:{},2:{},3:{},4:{},5:{},6:{}}
+
+            a=1
+            for ant in self.ants:
+                vehicle_tasklist[a]=ant.path
+                print('-----the task list of vehicle',a, 'is-----',ant.path)
+                a+=1
+
+
+
             #record the completion time of each iteration
             self.total_completion_time.append(self.total_distance)
 
@@ -328,11 +341,12 @@ class SCHEDULING(object):
             self.__update_pheromone_gragh()
             #print(u"迭代次数：", self.iter, self.total_completion_time)
             self.iter += 1
-            if self.iter == 100:
+            if self.iter == 2:
                 print(self.total_completion_time)
                 print(u"the best iteration：", self.best_iteration, u"The optimal total completion time：", self.best_distance)
                 self.__iteration_visulazation()
                 break
+            return vehicle_tasklist
 
     # update the pheromone
     def __update_pheromone_gragh(self):
@@ -367,10 +381,211 @@ class SCHEDULING(object):
             facet_kws=dict(sharex=False),
         )
         '''
+class Graph(object):
+    def __init__(self, nodes, init_graph):
+        self.nodes = nodes
+        self.graph = self.construct_graph(nodes, init_graph)
+
+    def construct_graph(self, nodes, init_graph):
+        '''
+        This method makes sure that the graph is symmetrical. In other words, if there's a path from node A to B with a value V, there needs to be a path from node B to node A with a value V.
+        '''
+        graph = {}
+        for node in nodes:
+            graph[node] = {}
+
+        graph.update(init_graph)
+
+        for node, edges in graph.items():
+            for adjacent_node, value in edges.items():
+                if graph[adjacent_node].get(node, False) == False:
+                    graph[adjacent_node][node] = value
+
+        return graph
+
+    def get_nodes(self):
+        "Returns the nodes of the graph."
+        return self.nodes
+
+    def get_outgoing_edges(self, node):
+        "Returns the neighbors of a node."
+        connections = []
+        for out_node in self.nodes:
+            if self.graph[node].get(out_node, False) != False:
+                connections.append(out_node)
+        return connections
+
+    def value(self, node1, node2):
+        "Returns the value of an edge between two nodes."
+        return self.graph[node1][node2]
+
+
+def dijkstra_algorithm(graph, start_node):
+    unvisited_nodes = list(graph.get_nodes())
+
+    # We'll use this dict to save the cost of visiting each node and update it as we move along the graph
+    shortest_path = {}
+
+    # We'll use this dict to save the shortest known path to a node found so far
+    previous_nodes = {}
+
+    # We'll use max_value to initialize the "infinity" value of the unvisited nodes
+    max_value = sys.maxsize
+    for node in unvisited_nodes:
+        shortest_path[node] = max_value
+    # However, we initialize the starting node's value with 0
+    shortest_path[start_node] = 0
+
+    # The algorithm executes until we visit all nodes
+    while unvisited_nodes:
+        # The code block below finds the node with the lowest score
+        current_min_node = None
+        for node in unvisited_nodes:  # Iterate over the nodes
+            if current_min_node == None:
+                current_min_node = node
+            elif shortest_path[node] < shortest_path[current_min_node]:
+                current_min_node = node
+
+        # The code block below retrieves the current node's neighbors and updates their distances
+        neighbors = graph.get_outgoing_edges(current_min_node)
+        for neighbor in neighbors:
+            tentative_value = shortest_path[current_min_node] + graph.value(current_min_node, neighbor)
+            if tentative_value < shortest_path[neighbor]:
+                shortest_path[neighbor] = tentative_value
+                # We also update the best path to the current node
+                previous_nodes[neighbor] = current_min_node
+
+        # After visiting its neighbors, we mark the node as "visited"
+        unvisited_nodes.remove(current_min_node)
+
+    return previous_nodes, shortest_path
+
+
+def generate_result(previous_nodes, shortest_path, start_node, target_node):
+    path = []
+    node = target_node
+
+    while node != start_node:
+        path.append(node)
+        node = previous_nodes[node]
+
+    # Add the start node manually
+    path.append(start_node)
+
+    print("We found the following best path with a value of {}.".format(shortest_path[target_node]))
+    print(" -> ",'Route' ,list(reversed(path)))
+
+def h(n):
+    H={}
+    for i in range(0,159):
+        H[i] =1
+    return H[n]
+
+def a_star_algorithm(graph,start, stop,speed,TaskID,endtime_lasttask):
+    # In this open_lst is a list of nodes which have not been visited, but who's
+    # neighbours haven't all been always inspected, It starts off with the start node
+    # And closed_lst is a list of nodes which have been visited
+    # and who's neighbors have been always inspected
+    open_lst = set([start])
+    closed_lst = set([])
+
+    arrive_time_lst = {}
+    for id in range(0,500):
+        arrive_time_lst[id] = {}
+
+    # dis_start has present distances from start to all other nodes
+    # the default value is +infinity
+    dis_start = {}
+    dis_start[start] = 0
+
+    # adjac_node contains an adjac mapping of all nodes
+    adjac_node = {}
+    adjac_node[start] = start
+
+    while len(open_lst) > 0:
+        n = None
+        # it will find a node with the lowest value of f() -
+        for v in open_lst:
+            if n == None or dis_start[v] + h(v) < dis_start[n] + h(n):
+                n = v
+        if n == None:
+            print('Path does not exist!')
+            return None
+        # if the current node is the stop
+        # then we start again from start
+        if n == stop:
+            reconst_path = []
+            while adjac_node[n] != n:
+                reconst_path.append(n)
+                n = adjac_node[n]
+
+            reconst_path.append(start)
+            reconst_path.reverse()
+
+            if n == start:
+                arrive_time_lst[TaskID][n]=2/speed + endtime_lasttask #previous route end time
+
+            for i in range(1,len(reconst_path)):
+                arrive_time_lst[TaskID][reconst_path[i]] = 2 / speed + arrive_time_lst[TaskID][reconst_path[i-1]]
+
+            print('Path found: {}'.format(reconst_path))
+            print('The arrive time of each point',arrive_time_lst[TaskID])
+            return reconst_path,arrive_time_lst[TaskID]
+
+        # for all the neighbors of the current node do
+        for m in graph.get_outgoing_edges(n):
+            # if the current node is not presentin both open_lst and closed_lst
+            # add it to open_lst and note n as it's par
+            if m not in open_lst and m not in closed_lst:
+                open_lst.add(m)
+                adjac_node[m] = n
+                dis_start[m] = dis_start[n] + graph.value(n,m)
+
+            # otherwise, check if it's quicker to first visit n, then m
+            # and if it is, update par data and poo data
+            # and if the node was in the closed_lst, move it to open_lst
+            else:
+                if dis_start[m] > dis_start[n] + graph.value(n,m):
+                    dis_start[m] = dis_start[n] + graph.value(n,m)
+                    adjac_node[m] = n
+
+                    if m in closed_lst:
+                        closed_lst.remove(m)
+                        open_lst.add(m)
+
+        # remove n from the open_lst, and add it to closed_lst
+        # because all of his neighbors were inspected
+        open_lst.remove(n)
+        closed_lst.add(n)
+
+    print('Path does not exist!')
+    return None
 
 
 # ----------- main -----------
 if __name__ == '__main__':
-    SCHEDULING()
-    outputfile.close()  # close后才能看到写入的数据
+    # input road map
+    Manufacturing_Graph = pd.read_excel('Graph.xlsx', sheet_name='Sheet1', usecols="A:C", skiprows=0, nrows=162,
+                                        dtype=object)
+    nodes = list(range(0, 159))
 
+    Node1 = Manufacturing_Graph['Node1'].tolist()
+    Node2 = Manufacturing_Graph['Node2'].tolist()
+
+    init_graph = {}
+    for node in nodes:
+        init_graph[node] = {}
+    for i in range(0, 161):
+        init_graph[Node1[i]][Node2[i]] = 2
+
+    graph = Graph(nodes, init_graph)
+    #scheduling the task
+    vehicle_tasklist=SCHEDULING()
+    #outputfile.close()  # close后才能看到写入的数据
+
+    #Route planning
+    for i in range(1,17,3):
+        reconst_path, arrive_time = a_star_algorithm(graph, Task_list.loc[i, 'Start node'], Task_list.loc[i, 'End node'],
+                                                     speed=1, TaskID=i, endtime_lasttask=0)
+        reconst_path_back, arrive_time_back=a_star_algorithm(graph, Task_list.loc[i, 'End node'], Task_list.loc[i, 'Start node'], speed=1, TaskID=0,
+                         endtime_lasttask=arrive_time[Task_list.loc[i, 'End node']])
